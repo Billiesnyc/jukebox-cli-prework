ba9e94ce9ba0e7ac62ce501886f65f42dd0e9c29
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 19:28:24 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,8 +12,8 @@ songs = [
 
 def help 
   puts "I accept the following commands:\n
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
+- help : displays this help message\n
+- list : displays a list of songs you can play\n
+- play : lets you choose a song to play\n
 - exit : exits this program"
 end
